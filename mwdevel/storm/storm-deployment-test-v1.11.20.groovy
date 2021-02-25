04945ec52eeb3d217a9319487d76c8ec60befd28
enricovianello <maverick17586@gmail.com>
2021-02-25 18:05:58 +0100

    Added storm-deployment-test-v1.11.20

---
+++ mwdevel/storm/storm-deployment-test-v1.11.20.groovy
@@ -0,0 +1,59 @@
+#!/usr/bin/env groovy
+
+@Library('sd')_
+def kubeLabel = getKubeLabel()
+def runner_job
+
+pipeline {
+  agent {
+    kubernetes {
+      label "${kubeLabel}"
+      cloud 'Kube mwdevel'
+      defaultContainer 'runner'
+      inheritFrom 'ci-template'
+    }
+  }
+
+  options {
+    buildDiscarder(logRotator(numToKeepStr: '5'))
+  }
+
+  triggers {
+    cron('@daily')
+  }
+
+  stages {
+    stage("test-pkg-storm-v1.11.20") {
+      steps {
+        script {
+          catchError{
+            runner_job = build job: "storm-deployment-tests/master", 
+              parameters: [
+                string(name: 'STORM_TARGET_RELEASE', value: "stable"),
+                string(name: 'VOMS_TARGET_RELEASE', value: "stable"),
+                string(name: 'PKG_STORM_BRANCH', value: "v1.11.20"),
+                string(name: 'PKG_VOMS_BRANCH', value: "none")
+              ], 
+              wait: true, 
+              propagate: false
+          }
+          step ([$class: 'CopyArtifact',
+            projectName: "storm-deployment-tests/master",
+            selector: [$class: 'SpecificBuildSelector', buildNumber: "${runner_job.number}"]
+          ])
+
+          archiveArtifacts 'all-in-one/centos7/output/**'
+          step([$class: 'RobotPublisher',
+            disableArchiveOutput: false,
+            logFileName: 'log.html',
+            otherFiles: '*.png',
+            outputFileName: 'output.xml',
+            outputPath: "all-in-one/centos7/output/reports",
+            passThreshold: 100,
+            reportFileName: 'report.html',
+            unstableThreshold: 90])
+        }
+      }
+    }
+  }
+}
